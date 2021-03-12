e0fbd517820b5a23c22542f5efef909fb57a73b2
André Z. Vitorelli <andrezvitorelli@gmail.com>
2021-03-12 10:37:05 +0100

    bugfixes

--- autometacal/__init__.py
+++ autometacal/__init__.py
@@ -1,2 +1,2 @@
 __version__ = "0.0.0"
-from . import data
+from . import data, galaxies
--- autometacal/data.py
+++ autometacal/data.py
@@ -25,15 +25,15 @@ class GalGen(tfds.core.GeneratorBasedBuilder):
                channels=1,
                galsim_drawing_method = "no_pixel",
                galsim_interpolator = "linear"):
-        """
-        Inputs:
-            data_file: table from where shape parameters, size and snr are read
-            drawer: a galaxy drawer method from galaxies.py
-            stamp_size : the size of the square images to be generated
-            channels: how many channels (different filters) for each galaxy
-            galsim_drawing_method: see galsim
-            galsim_interpolator: not yet implemented
-        """
+    """
+      Inputs:
+        data_file: table from where shape parameters, size and snr are read
+        drawer: a galaxy drawer method from galaxies.py
+        stamp_size : the size of the square images to be generated
+        channels: how many channels (different filters) for each galaxy
+        galsim_drawing_method: see galsim
+        galsim_interpolator: not yet implemented
+    """
     self.data = Table.read(data_file)
     self.stamp_size = stamp_size
     self.channels = channels
@@ -74,7 +74,7 @@ class GalGen(tfds.core.GeneratorBasedBuilder):
              }
 
   def _generate_examples(self, data):
-      """Yields examples."""
+    """Yields examples."""
     for i, galaxy in enumerate(data):
       image = self.drawer(galaxy,None)
       g1, g2 = galaxy['g1'], galaxy['g2']
@@ -105,8 +105,10 @@ class GalGenCOSMOS(tfds.core.GeneratorBasedBuilder):
     extracted_path = dl_manager.download_and_extract('https://zenodo.org/record/3242143/files/COSMOS_25.2_training_sample.tar.gz')
     cat = galsim.COSMOSCalog('COSMOS_25.2_training_sample')
     split = 0.7
-    trainlist = [0:round(split*len(cat))]
-    testlist = [0:round(split*len(cat))]
+    indexes = range(len(cat))
+    np.random.shuffle(indexes)
+    trainlist = indexes[0:round(split*len(cat))]
+    testlist = indexes[0:round(split*len(cat))]
     # dl_manager returns pathlib-like objects with `path.read_text()`,
     # `path.iterdir()`,...
     return {
@@ -114,7 +116,7 @@ class GalGenCOSMOS(tfds.core.GeneratorBasedBuilder):
         'test': self._generate_examples(cat,testlist),
     }
 
-  def _generate_examples(self, cat, sublist) -> Iterator[Tuple[Key, Example]]:
+  def _generate_examples(self, cat, sublist):
     """Generator of examples for each split."""
     for i in sublist:
       gal = cat.makeGalaxy(i)
@@ -151,13 +153,14 @@ class GalGenHSC(tfds.core.GeneratorBasedBuilder):
   def _split_generators(self, dl_manager: tfds.download.DownloadManager):
     """Download the data and define splits."""
     data_choices = ["parent_best_processed", "parent_median_processed", "parent_worst_processed"]
-    
+
 
     extracted_path = dl_manager.download_and_extract(f"https://hsc-release.mtk.nao.ac.jp/archive/pdr1_incremental/{data_choices[0]}.tar.gz")
     cat = galsim.RealGalaxyCatalog(data_choices[0]) ##?
-    split = 0.7
-    trainlist = [0:round(split*len(cat))]
-    testlist = [0:round(split*len(cat))]
+    indexes = range(len(cat))
+    np.random.shuffle(indexes)
+    trainlist = indexes[0:round(split*len(cat))]
+    testlist = indexes[0:round(split*len(cat))]
     # dl_manager returns pathlib-like objects with `path.read_text()`,
     # `path.iterdir()`,...
     return {
@@ -165,7 +168,7 @@ class GalGenHSC(tfds.core.GeneratorBasedBuilder):
         'test': self._generate_examples(cat,testlist),
     }
 
-  def _generate_examples(self, cat, sublist) -> Iterator[Tuple[Key, Example]]:
+  def _generate_examples(self, cat, sublist):
     """Generator of examples for each split."""
     for i in sublist:
       gal = cat.makeGalaxy(i)
