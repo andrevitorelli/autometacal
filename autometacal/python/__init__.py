634e2610994c82368bef8a17f54b5550856f9308
André Z. Vitorelli <andrezvitorelli@gmail.com>
2022-06-30 18:59:12 +0200

    finishing experimental setup

--- autometacal/python/__init__.py
+++ autometacal/python/__init__.py
@@ -2,4 +2,7 @@ import autometacal.python.datasets as datasets
 import autometacal.python.tf_ngmix as tf_ngmix
 from autometacal.python.metacal import generate_mcal_image, get_metacal_response, get_metacal_response_finitediff
 from autometacal.python.fitting import fit_multivariate_gaussian, get_ellipticity
-from autometacal.python.moments import get_moment_ellipticities, gaussian_moments
+from autometacal.python.gaussmom import get_moment_ellipticities, moments
+from autometacal.python.galflow import shear, dilate
+from autometacal.python.util import noiseless_real_mcal_image
+import autometacal.python.metacal_psf as metacal_psf
