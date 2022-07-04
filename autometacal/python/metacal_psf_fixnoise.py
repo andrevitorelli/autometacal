b21db31c3cc6c8bf0f547b2796328debae3f26fc
André Z. Vitorelli <andrezvitorelli@gmail.com>
2022-07-04 15:58:14 +0200

    experimental work

--- autometacal/python/metacal_psf_fixnoise.py
+++ autometacal/python/metacal_psf_fixnoise.py
@@ -109,7 +109,7 @@ def get_metacal_response(gal_images,
   psf_images = tf.convert_to_tensor(psf_images, dtype=tf.float32)
   batch_size, _ , _ = gal_images.get_shape().as_list()
   gs = tf.zeros([batch_size,4])
-  epsf = method(reconvolution_psf_image)
+  epsf = method(reconvolution_psf_image) #doesn't work - biased
   with tf.GradientTape() as tape:
     tape.watch(gs)
     # Measure ellipticity under metacal
@@ -124,8 +124,130 @@ def get_metacal_response(gal_images,
     
     e = method(mcal_image)
 
-  #Rpsf = tape.batch_jacobian(e, gp)
-  #R = tape2.batch_jacobian(e, g)
   Rs = tape.batch_jacobian(e, gs)
   R, Rpsf = Rs[...,0:2], Rs[...,2:4]
   return e, epsf, R, Rpsf
+
+
+def get_metacal_response_finitediff(gal_image,psf_image,reconv_psf_image,noise,step,step_psf,method):
+  """
+  Gets shear response as a finite difference operation, 
+  instead of automatic differentiation.
+  """
+  
+  #noshear
+  img0s = generate_mcal_image(
+    gal_image,
+    psf_image,
+    reconv_psf_image,
+    [[0,0]],[[0,0]]
+  )  
+  
+  #############SHEAR RESPONSE
+  #1p
+  img1p = generate_mcal_image(
+    gal_image,
+    psf_image,
+    reconv_psf_image,
+    [[step,0]],[[0,0]]
+  )+ generate_fixnoise()
+  
+  #1m
+  img1m = generate_mcal_image(
+    gal_image,
+    psf_image,
+    reconv_psf_image,
+    [[-step,0]],[[0,0]]
+  )+ generate_fixnoise()
+  
+  #2p
+  img2p = generate_mcal_image(
+    gal_image,
+    psf_image,
+    reconv_psf_image,
+    [[0,step]],[[0,0]]
+  )+ generate_fixnoise()
+  
+  #2m
+  img2m = generate_mcal_image(
+    gal_image,
+    psf_image,
+    reconv_psf_image,
+    [[0,-step]],[[0,0]]
+  )+ generate_fixnoise()
+  
+  g0s = method(img0s)
+  g1p = method(img1p)
+  g1m = method(img1m)
+  g2p = method(img2p)
+  g2m = method(img2m)
+  
+  R11 = (g1p[:,0]-g1m[:,0])/(2*step)
+  R21 = (g1p[:,1]-g1m[:,1])/(2*step) 
+  R12 = (g2p[:,0]-g2m[:,0])/(2*step)
+  R22 = (g2p[:,1]-g2m[:,1])/(2*step)
+  
+  R = tf.transpose(tf.convert_to_tensor(
+    [[R11,R21],
+     [R12,R22]],dtype=tf.float32)
+  ) 
+  
+  ####################PSF RESPONSE  
+  #1p_psf
+  img1p_psf = generate_mcal_image(
+    gal_image,
+    psf_image,
+    reconv_psf_image,
+    [[0,0]],[[step,0]]
+  )+ generate_fixnoise()
+
+  #1m_psf
+  img1m_psf = generate_mcal_image(
+    gal_image,
+    psf_image,
+    reconv_psf_image,
+    [[0,0]],[[-step,0]]
+  )+ generate_fixnoise()
+  #2p_psf
+  img2p_psf = generate_mcal_image(
+    gal_image,
+    psf_image,
+    reconv_psf_image,
+    [[0,0]][[0,step]]
+  )+ generate_fixnoise()
+  #2m_psf
+  img2m_psf = generate_mcal_image(
+    gal_image,
+    psf_image,
+    reconv_psf_image,
+    [[0,0]][[0,-step]]
+  )+ generate_fixnoise()
+  
+  g1p_psf = method(img1p_psf)
+  g1m_psf = method(img1m_psf)
+  g2p_psf = method(img2p_psf)
+  g2m_psf = method(img2m_psf)
+ 
+  Rpsf11 = (g1p_psf[:,0]-g1m_psf[:,0])/(2*step_psf)
+  Rpsf21 = (g1p_psf[:,1]-g1m_psf[:,1])/(2*step_psf) 
+  Rpsf12 = (g2p_psf[:,0]-g2m_psf[:,0])/(2*step_psf)
+  Rpsf22 = (g2p_psf[:,1]-g2m_psf[:,1])/(2*step_psf)
+ 
+  R = tf.transpose(tf.convert_to_tensor(
+    [[R11,R21],
+     [R12,R22]],dtype=tf.float32)
+  )
+  
+  ellip_dict = {
+    'noshear':g0s,
+    '1p':g1p,
+    '1m':g1m,
+    '2p':g2p,
+    '2m':g2m,
+    '1p_psf':g1p_psf,
+    '1m_psf':g1m_psf,
+    '2p_psf':g2p_psf,
+    '2m_psf':g2m_psf,
+  } 
+
+  return ellip_dict, R, Rpsf
